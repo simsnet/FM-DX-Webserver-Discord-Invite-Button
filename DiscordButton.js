d466edb87eda8c3f1c9f703021d3a4cfe9eb148b
Glenn <simstvnet@gmail.com>
2025-12-07 18:09:17 -0500

    Initial commit

---
+++ DiscordButton.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Discord Button',
+    version: '1.0.0',
+    author: 'simsnet',
+    frontEndPath: 'DiscordButton/DiscordButtonCore.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+}
